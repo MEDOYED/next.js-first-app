7ef074a02f6cdcdd4efff021ae70f631c665081e
MEDOYED <mmax26072005@gmail.com>
2024-06-02 21:06:28 +0200

    layout Japan page without components

--- app/japan/page.tsx
+++ app/japan/page.tsx
@@ -4,59 +4,32 @@ import styles from "./page.module.css";
 
 export default function Japan() {
   return (
-    // <div className="container">
-    //   <ul className="japan__list">
-    //     <li className="japan__item">
-    //       <img className="japan__img" src="/works/1.png" alt="" />
-    //       <p className="japan__description">description for the first image</p>
-    //     </li>
-    //     <li className="japan__item">
-    //       <img className="japan__img" src="/works/1.png" alt="" />
-    //       <p className="japan__description">description for the first image</p>
-    //     </li>
-    //     <li className="japan__item">
-    //       <img className="japan__img" src="/works/1.png" alt="" />
-    //       <p className="japan__description">description for the first image</p>
-    //     </li>
-    //     <li className="japan__item">
-    //       <img className="japan__img" src="/works/1.png" alt="" />
-    //       <p className="japan__description">description for the first image</p>
-    //     </li>
-    //     <li className="japan__item">
-    //       <img className="japan__img" src="/works/1.png" alt="" />
-    //       <p className="japan__description">description for the first image</p>
-    //     </li>
-    //     <li className="japan__item">
-    //       <img className="japan__img" src="/works/1.png" alt="" />
-    //       <p className="japan__description">description for the first image</p>
-    //     </li>
-    //   </ul>
-    // </div>
     <div className={styles.container}>
       <ul className={styles.japan__list}>
+        
         <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/works/1.png" alt="" />
-          <p className={styles.japan__description}>description for the first image</p>
+          <img className={styles.japan__img} src="/japan-img/1.jpg" alt="Fushimi-inari Temple" />
+          <p className={styles.japan__description}>Fushimi-inari Temple (Kyoto)</p>
         </li>
         <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/works/1.png" alt="" />
-          <p className={styles.japan__description}>description for the first image</p>
+          <img className={styles.japan__img} src="/japan-img/2.jpg" alt="Mount Fuji" />
+          <p className={styles.japan__description}>Mount Fuji (Honshu Island)</p>
         </li>
         <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/works/1.png" alt="" />
-          <p className={styles.japan__description}>description for the first image</p>
+          <img className={styles.japan__img} src="/japan-img/3.jpg" alt="Sky Tree" />
+          <p className={styles.japan__description}>Sky Tree (Tokyo)</p>
         </li>
         <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/works/1.png" alt="" />
-          <p className={styles.japan__description}>description for the first image</p>
+          <img className={styles.japan__img} src="/japan-img/4.jpg" alt="Sagano Bamboo Forest" />
+          <p className={styles.japan__description}>Sagano Bamboo Forest (Kyoto)</p>
         </li>
         <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/works/1.png" alt="" />
-          <p className={styles.japan__description}>description for the first image</p>
+          <img className={styles.japan__img} src="/japan-img/5.jpg" alt="" />
+          <p className={styles.japan__description}>Jigokudani Snow Monkey (Nagano)</p>
         </li>
         <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/works/1.png" alt="" />
-          <p className={styles.japan__description}>description for the first image</p>
+          <img className={styles.japan__img} src="/japan-img/6.jpg" alt="" />
+          <p className={styles.japan__description}>Itsukushima-jinja Temple (Miyajima Island)</p>
         </li>
       </ul>
     </div>
