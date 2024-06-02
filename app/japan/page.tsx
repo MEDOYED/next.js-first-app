19d0c5571b4a8959faf56d3429f7f0d065b6b75e
MEDOYED <mmax26072005@gmail.com>
2024-06-02 11:36:33 +0200

    creating next-app + header + japan-page

---
+++ app/japan/page.tsx
@@ -0,0 +1,64 @@
+// import Link from "next/link"
+// import Image from "next/image"
+import styles from "./page.module.css";
+
+export default function Japan() {
+  return (
+    // <div className="container">
+    //   <ul className="japan__list">
+    //     <li className="japan__item">
+    //       <img className="japan__img" src="/works/1.png" alt="" />
+    //       <p className="japan__description">description for the first image</p>
+    //     </li>
+    //     <li className="japan__item">
+    //       <img className="japan__img" src="/works/1.png" alt="" />
+    //       <p className="japan__description">description for the first image</p>
+    //     </li>
+    //     <li className="japan__item">
+    //       <img className="japan__img" src="/works/1.png" alt="" />
+    //       <p className="japan__description">description for the first image</p>
+    //     </li>
+    //     <li className="japan__item">
+    //       <img className="japan__img" src="/works/1.png" alt="" />
+    //       <p className="japan__description">description for the first image</p>
+    //     </li>
+    //     <li className="japan__item">
+    //       <img className="japan__img" src="/works/1.png" alt="" />
+    //       <p className="japan__description">description for the first image</p>
+    //     </li>
+    //     <li className="japan__item">
+    //       <img className="japan__img" src="/works/1.png" alt="" />
+    //       <p className="japan__description">description for the first image</p>
+    //     </li>
+    //   </ul>
+    // </div>
+    <div className={styles.container}>
+      <ul className={styles.japan__list}>
+        <li className={styles.japan__item}>
+          <img className={styles.japan__img} src="/works/1.png" alt="" />
+          <p className={styles.japan__description}>description for the first image</p>
+        </li>
+        <li className={styles.japan__item}>
+          <img className={styles.japan__img} src="/works/1.png" alt="" />
+          <p className={styles.japan__description}>description for the first image</p>
+        </li>
+        <li className={styles.japan__item}>
+          <img className={styles.japan__img} src="/works/1.png" alt="" />
+          <p className={styles.japan__description}>description for the first image</p>
+        </li>
+        <li className={styles.japan__item}>
+          <img className={styles.japan__img} src="/works/1.png" alt="" />
+          <p className={styles.japan__description}>description for the first image</p>
+        </li>
+        <li className={styles.japan__item}>
+          <img className={styles.japan__img} src="/works/1.png" alt="" />
+          <p className={styles.japan__description}>description for the first image</p>
+        </li>
+        <li className={styles.japan__item}>
+          <img className={styles.japan__img} src="/works/1.png" alt="" />
+          <p className={styles.japan__description}>description for the first image</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
