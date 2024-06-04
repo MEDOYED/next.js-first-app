2af390c16ddb351bb68eabdced308606f887a6a0
MEDOYED <mmax26072005@gmail.com>
2024-06-04 10:30:59 +0200

    made Japan page by component CardSights

--- app/components/CardSights.tsx
+++ app/components/CardSights.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../styles/page.module.css';
-// import Japan from '../japan/page';
 
 interface Card {
   src: string;
--- app/japan/page.tsx
+++ app/japan/page.tsx
@@ -1,35 +1,20 @@
-// import Link from "next/link"
-// import Image from "next/image"
-import styles from "./page.module.css";
+import CardPage from "../components/CardSights";
+import styles from "../styles/page.module.css";
+
+const cardsJapan = [
+  { src: '/japan-img/1.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+  { src: '/japan-img/2.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+  { src: '/japan-img/3.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+  { src: '/japan-img/4.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+  { src: '/japan-img/5.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+  { src: '/japan-img/6.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+]
 
 export default function Japan() {
   return (
     <div className={styles.container}>
-      <ul className={styles.japan__list}>
-        <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/japan-img/1.jpg" alt="Fushimi-inari Temple" />
-          <p className={styles.japan__description}>Fushimi-inari Temple (Kyoto)</p>
-        </li>
-        <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/japan-img/2.jpg" alt="Mount Fuji" />
-          <p className={styles.japan__description}>Mount Fuji (Honshu Island)</p>
-        </li>
-        <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/japan-img/3.jpg" alt="Sky Tree" />
-          <p className={styles.japan__description}>Sky Tree (Tokyo)</p>
-        </li>
-        <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/japan-img/4.jpg" alt="Sagano Bamboo Forest" />
-          <p className={styles.japan__description}>Sagano Bamboo Forest (Kyoto)</p>
-        </li>
-        <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/japan-img/5.jpg" alt="" />
-          <p className={styles.japan__description}>Jigokudani Snow Monkey (Nagano)</p>
-        </li>
-        <li className={styles.japan__item}>
-          <img className={styles.japan__img} src="/japan-img/6.jpg" alt="" />
-          <p className={styles.japan__description}>Itsukushima-jinja Temple (Miyajima Island)</p>
-        </li>
+      <ul className={styles.list}>
+        <CardPage cards={cardsJapan} />
       </ul>
     </div>
   );
