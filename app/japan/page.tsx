a20abe462413e296697ea2ed4dbe2d563e0d3290
MEDOYED <mmax26072005@gmail.com>
2024-06-04 10:40:01 +0200

    modified Japan page

--- app/japan/page.tsx
+++ app/japan/page.tsx
@@ -3,11 +3,11 @@ import styles from "../styles/page.module.css";
 
 const cardsJapan = [
   { src: '/japan-img/1.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
-  { src: '/japan-img/2.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
-  { src: '/japan-img/3.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
-  { src: '/japan-img/4.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
-  { src: '/japan-img/5.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
-  { src: '/japan-img/6.jpg', alt: 'Fushimi-inari Temple', description: 'Fushimi-inari Temple (Kyoto)' },
+  { src: '/japan-img/2.jpg', alt: 'Mount Fuji', description: 'Mount Fuji (Honshu Island)' },
+  { src: '/japan-img/3.jpg', alt: 'Sky Tree', description: 'Sky Tree (Tokyo)' },
+  { src: '/japan-img/4.jpg', alt: 'Sagano Bamboo Forest', description: 'Sagano Bamboo Forest (Kyoto)' },
+  { src: '/japan-img/5.jpg', alt: 'Jigokudani Snow Monkey', description: 'Jigokudani Snow Monkey (Nagano)' },
+  { src: '/japan-img/6.jpg', alt: 'Itsukushima-jinja Temple', description: 'Itsukushima-jinja Temple (Miyajima Island)' },
 ]
 
 export default function Japan() {
